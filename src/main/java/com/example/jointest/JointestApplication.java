7b4218d5a156a0c20753fce8ae918a22d78f61b3
root123 <1071215950@qq.com>
2019-07-12 13:13:03 +0800

    add;新增hello方法

---
+++ src/main/java/com/example/jointest/JointestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jointest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JointestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JointestApplication.class, args);
+    }
+
+}
